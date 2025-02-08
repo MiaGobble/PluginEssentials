06b6a4068167907c5ce6fb2348b86fcd8e85098f
Mia Gobble <miagobble@duck.com>
2025-02-08 10:26:43 -0500

    Migrated Loading to 0.3

--- src/StudioComponents/Loading.lua
+++ src/StudioComponents/Loading.lua
@@ -1,11 +1,13 @@
--- Written by @boatbomber
+-- Constants
+local COMPONENT_ONLY_PROPERTIES = {
+	"Enabled",
+}
 
+-- Imports
 local Plugin = script:FindFirstAncestorWhichIsA("Plugin") or game
 local Fusion = require(Plugin:FindFirstChild("Fusion", true))
-
 local StudioComponents = script.Parent
 local StudioComponentsUtil = StudioComponents:FindFirstChild("Util")
-
 local getMotionState = require(StudioComponentsUtil.getMotionState)
 local themeProvider = require(StudioComponentsUtil.themeProvider)
 local stripProps = require(StudioComponentsUtil.stripProps)
@@ -13,31 +15,20 @@ local constants = require(StudioComponentsUtil.constants)
 local getState = require(StudioComponentsUtil.getState)
 local unwrap = require(StudioComponentsUtil.unwrap)
 local types = require(StudioComponentsUtil.types)
-
-local Computed = Fusion.Computed
-local Hydrate = Fusion.Hydrate
-local Value = Fusion.Value
-local New = Fusion.New
-local Observer = Fusion.Observer
+local Scope = Fusion.scoped(Fusion)
 local Children = Fusion.Children
-local Cleanup = Fusion.Cleanup
+local OnEvent = Fusion.OnEvent
 
-local COMPONENT_ONLY_PROPERTIES = {
-	"Enabled",
-}
-
+-- Types Extended
 type LoadingProperties = {
 	Enabled: types.CanBeState<boolean>?,
 	[any]: any,
 }
 
-local cos = math.cos
-local clock = os.clock
-local pi4 = 12.566370614359172 --4*pi
-
 return function(props: LoadingProperties): Frame
+	local Scope = Scope:innerScope()
 	local isEnabled = getState(props.Enabled, true)
-	local time = Value(0)
+	local timePassed = Scope:Value(0)
 
 	local animThread = nil
 
@@ -50,16 +41,17 @@ return function(props: LoadingProperties): Frame
 		end
 
 		animThread = task.defer(function()
-			local startTime = clock()
+			local startTime = os.clock()
 			while unwrap(isEnabled) do
-				time:set(clock()-startTime)
+				timePassed:set(os.clock() - startTime)
 				task.wait(1/25) -- Springs will smooth out the motion so we needn't bother with high refresh rate here
+				-- TODO: iGottic wants to remove the task yield stuff
 			end
 		end)
 	end
 
 	startMotion()
-	local observeDisconnect = Observer(isEnabled):onChange(startMotion)
+	local observeDisconnect = Scope:Observer(isEnabled):onChange(startMotion)
 
 	local function haltAnim()
 		observeDisconnect()
@@ -72,63 +64,71 @@ return function(props: LoadingProperties): Frame
 	local light = themeProvider:GetColor(Enum.StudioStyleGuideColor.Light, Enum.StudioStyleGuideModifier.Default)
 	local accent = themeProvider:GetColor(Enum.StudioStyleGuideColor.DialogMainButton, Enum.StudioStyleGuideModifier.Default)
 
-	local alphaA = Computed(function()
-		local t = (unwrap(time) + 0.25) * pi4
-		return (cos(t)+1)/2
-	end)
-	local alphaB = Computed(function()
-		local t = unwrap(time) * pi4
-		return (cos(t)+1)/2
+	local alphaA = Scope:Computed(function(use)
+		local t = (unwrap(timePassed, use) + 0.25) * math.pi * 4
+		return (math.cos(t) + 1) / 2
 	end)
 
-	local colorA = getMotionState(Computed(function()
-		return unwrap(light):Lerp(unwrap(accent), unwrap(alphaA))
+	local alphaB = Scope:Computed(function(use)
+		local t = unwrap(timePassed, use) * math.pi * 4
+		return (math.cos(t) + 1) / 2
+	end)
+
+	local colorA = getMotionState(Scope:Computed(function(use)
+		return unwrap(light, use):Lerp(unwrap(accent, use), unwrap(alphaA, use))
 	end), "Spring", 40)
 
-	local colorB = getMotionState(Computed(function()
-		return unwrap(light):Lerp(unwrap(accent), unwrap(alphaB))
+	local colorB = getMotionState(Scope:Computed(function(use)
+		return unwrap(light, use):Lerp(unwrap(accent, use), unwrap(alphaB, use))
 	end), "Spring", 40)
 
-	local sizeA = getMotionState(Computed(function()
-		local alpha = unwrap(alphaA)
+	local sizeA = getMotionState(Scope:Computed(function(use)
+		local alpha = unwrap(alphaA, use)
 		return UDim2.fromScale(
 			0.2,
 			0.5 + alpha*0.5
 		)
 	end), "Spring", 40)
 
-	local sizeB = getMotionState(Computed(function()
-		local alpha = unwrap(alphaB)
+	local sizeB = getMotionState(Scope:Computed(function(use)
+		local alpha = unwrap(alphaB, use)
 		return UDim2.fromScale(
 			0.2,
 			0.5 + alpha*0.5
 		)
 	end), "Spring", 40)
 
-	local frame = New "Frame" {
+	local frame; frame = Scope:New "Frame" {
 		Name = "Loading",
 		BackgroundTransparency = 1,
 		Size = UDim2.new(0,constants.TextSize*4, 0,constants.TextSize*1.5),
 		Visible = isEnabled,
 		ClipsDescendants = true,
-		[Cleanup] = haltAnim,
+
+		[OnEvent "AncestryChanged"] = function()
+			if not frame.Parent then
+				Scope:doCleanup()
+			end
+		end,
 
 		[Children] = {
-			New "Frame" {
+			Scope:New "Frame" {
 				Name = "Bar1",
 				BackgroundColor3 = colorA,
 				Size = sizeA,
 				Position = UDim2.fromScale(0.02, 0.5),
 				AnchorPoint = Vector2.new(0,0.5),
 			},
-			New "Frame" {
+
+			Scope:New "Frame" {
 				Name = "Bar2",
 				BackgroundColor3 = colorB,
 				Size = sizeB,
 				Position = UDim2.fromScale(0.5, 0.5),
 				AnchorPoint = Vector2.new(0.5,0.5),
 			},
-			New "Frame" {
+
+			Scope:New "Frame" {
 				Name = "Bar3",
 				BackgroundColor3 = colorA,
 				Size = sizeA,
@@ -138,6 +138,8 @@ return function(props: LoadingProperties): Frame
 		}
 	}
 
+	table.insert(Scope, haltAnim)
+
 	local hydrateProps = stripProps(props, COMPONENT_ONLY_PROPERTIES)
-	return Hydrate(frame)(hydrateProps)
+	return Scope:Hydrate(frame)(hydrateProps)
 end
