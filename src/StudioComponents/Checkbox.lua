8583706a4d2bcdbb939aeb4ec78bfce87ca0c882
Mia Gobble <miagobble@duck.com>
2025-02-07 21:08:18 -0500

    Converted checkbox to 0.3

--- src/StudioComponents/Checkbox.lua
+++ src/StudioComponents/Checkbox.lua
@@ -1,29 +1,4 @@
--- Roact version by @sircfenner
--- Ported to Fusion by @YasuYoshida
-
-local Plugin = script:FindFirstAncestorWhichIsA("Plugin") or game
-local Fusion = require(Plugin:FindFirstChild("Fusion", true))
-
-local StudioComponents = script.Parent
-local StudioComponentsUtil = StudioComponents:FindFirstChild("Util")
-
-local BoxBorder = require(StudioComponents.BoxBorder)
-
-local getMotionState = require(StudioComponentsUtil.getMotionState)
-local themeProvider = require(StudioComponentsUtil.themeProvider)
-local getModifier = require(StudioComponentsUtil.getModifier)
-local constants = require(StudioComponentsUtil.constants)
-local getState = require(StudioComponentsUtil.getState)
-local unwrap = require(StudioComponentsUtil.unwrap)
-local types = require(StudioComponentsUtil.types)
-
-local New = Fusion.New
-local Value = Fusion.Value
-local OnEvent = Fusion.OnEvent
-local Computed = Fusion.Computed
-local Children = Fusion.Children
-local Hydrate = Fusion.Hydrate
-
+-- Constants
 local INDICATOR_IMAGE = "rbxassetid://6652838434"
 local COMPONENT_ONLY_PROPERTIES = {
 	"OnChange",
@@ -33,6 +8,24 @@ local COMPONENT_ONLY_PROPERTIES = {
 	"Text",
 }
 
+-- Imports
+local Plugin = script:FindFirstAncestorWhichIsA("Plugin") or game
+local Fusion = require(Plugin:FindFirstChild("Fusion", true))
+local StudioComponents = script.Parent
+local StudioComponentsUtil = StudioComponents:FindFirstChild("Util")
+local BoxBorder = require(StudioComponents.BoxBorder)
+local getMotionState = require(StudioComponentsUtil.getMotionState)
+local themeProvider = require(StudioComponentsUtil.themeProvider)
+local getModifier = require(StudioComponentsUtil.getModifier)
+local constants = require(StudioComponentsUtil.constants)
+local getState = require(StudioComponentsUtil.getState)
+local unwrap = require(StudioComponentsUtil.unwrap)
+local types = require(StudioComponentsUtil.types)
+local Scope = Fusion.scoped(Fusion)
+local OnEvent = Fusion.OnEvent
+local Children = Fusion.Children
+
+-- Types Extended
 type CheckboxProperties = {
 	OnChange: ((newValue: boolean) -> nil)?,
 	Alignment: (Enum.HorizontalAlignment | types.StateObject<Enum.HorizontalAlignment>)?,
@@ -45,10 +38,10 @@ type CheckboxProperties = {
 return function(props: CheckboxProperties): Frame
 	local currentValue = getState(props.Value, true)
 	local isEnabled = getState(props.Enabled, true)
-	local isHovering = Value(false)
+	local isHovering = Scope:Value(false)
 
-	local isIndeterminate = Computed(function()
-		return unwrap(currentValue)==nil
+	local isIndeterminate = Scope:Computed(function(use)
+		return unwrap(currentValue, use)==nil
 	end)
 
 	local mainModifier = getModifier({
@@ -63,22 +56,22 @@ return function(props: CheckboxProperties): Frame
 
 	local checkFieldIndicatorColor = themeProvider:GetColor(Enum.StudioStyleGuideColor.CheckedFieldIndicator, mainModifier)
 
-	local boxHorizontalScale = Computed(function()
-		local currentAlignment = unwrap(props.Alignment) or Enum.HorizontalAlignment.Left
+	local boxHorizontalScale = Scope:Computed(function(use)
+		local currentAlignment = unwrap(props.Alignment, use) or Enum.HorizontalAlignment.Left
 		return if currentAlignment==Enum.HorizontalAlignment.Right then 1 else 0
 	end)
 
-	local textHorizontalScale = Computed(function()
-		return if unwrap(boxHorizontalScale)==1 then 0 else 1
+	local textHorizontalScale = Scope:Computed(function(use)
+		return if unwrap(boxHorizontalScale, use)==1 then 0 else 1
 	end)
 
-	local newCheckboxFrame = New "Frame" {
+	local newCheckboxFrame = Scope:New "Frame" {
 		Name = "Checkbox",
 		Size = UDim2.new(1, 0, 0, 15),
 		BackgroundTransparency = 1,
 
 		[Children] = {
-			New "TextButton" {
+			Scope:New "TextButton" {
 				Text = "",
 				Active = true,
 				Name = "CheckBoxInput",
@@ -112,43 +105,43 @@ return function(props: CheckboxProperties): Frame
 			BoxBorder {
 				Color = getMotionState(themeProvider:GetColor(Enum.StudioStyleGuideColor.CheckedFieldBorder, mainModifier), "Spring", 40),
 
-				[Children] = New "Frame" {
+				[Children] = Scope:New "Frame" {
 					Name = "Box",
-					AnchorPoint = Computed(function()
+					AnchorPoint = Scope:Computed(function()
 						return Vector2.new(unwrap(boxHorizontalScale), 0)
 					end),
-					Position = Computed(function()
+					Position = Scope:Computed(function()
 						return UDim2.fromScale(unwrap(boxHorizontalScale), 0)
 					end),
 					BackgroundColor3 = getMotionState(themeProvider:GetColor(Enum.StudioStyleGuideColor.CheckedFieldBackground, backModifier), "Spring", 40),
 					Size = UDim2.fromOffset(15, 15),
 
-					[Children] = New "ImageLabel" {
+					[Children] = Scope:New "ImageLabel" {
 						AnchorPoint = Vector2.new(.5, .5),
-						Visible = Computed(function()
-							return unwrap(currentValue)~=false
+						Visible = Scope:Computed(function(use)
+							return unwrap(currentValue, use)~=false
 						end),
 						Name = "Indicator",
 						BackgroundTransparency = 1,
 						Size = UDim2.fromOffset(13, 13),
 						Position = UDim2.fromScale(.5, .5),
 						Image = INDICATOR_IMAGE,
-						ImageColor3 = getMotionState(Computed(function()
+						ImageColor3 = getMotionState(Scope:Computed(function(use)
 							local indicatorColor = unwrap(checkFieldIndicatorColor)
-							return if unwrap(isIndeterminate) then Color3.fromRGB(255, 255, 255) else indicatorColor
+							return if unwrap(isIndeterminate, use) then Color3.fromRGB(255, 255, 255) else indicatorColor
 						end), "Spring", 40),
-						ImageRectOffset = Computed(function()
-							if unwrap(isIndeterminate) then
-								return if unwrap(themeProvider.IsDark) then Vector2.new(13, 0) else Vector2.new(26, 0)
+						ImageRectOffset = Scope:Computed(function(use)
+							if unwrap(isIndeterminate, use) then
+								return if unwrap(themeProvider.IsDark, use) then Vector2.new(13, 0) else Vector2.new(26, 0)
 							end
 							return Vector2.new(0, 0)
 						end),
 						ImageRectSize = Vector2.new(13, 13),
 
-						[Children] = Computed(function()
-							local useCurvedBoxes = unwrap(constants.CurvedBoxes)
+						[Children] = Scope:Computed(function(use)
+							local useCurvedBoxes = unwrap(constants.CurvedBoxes, use)
 							if useCurvedBoxes then
-								return New "UICorner" {
+								return Scope:New "UICorner" {
 									CornerRadius = constants.CornerRadius
 								}
 							end
@@ -156,19 +149,19 @@ return function(props: CheckboxProperties): Frame
 					}
 				}
 			},
-			Computed(function()
+			Scope:Computed(function()
 				if props.Text then
-					return New "TextLabel" {
+					return Scope:New "TextLabel" {
 						BackgroundTransparency = 1,
-						AnchorPoint = Computed(function()
-							return Vector2.new(unwrap(textHorizontalScale), 0)
+						AnchorPoint = Scope:Computed(function(use)
+							return Vector2.new(unwrap(textHorizontalScale, use), 0)
 						end),
-						Position = Computed(function()
-							return UDim2.fromScale(unwrap(textHorizontalScale), 0)
+						Position = Scope:Computed(function(use)
+							return UDim2.fromScale(unwrap(textHorizontalScale, use), 0)
 						end),
 						Size = UDim2.new(1, -20, 1, 0),
-						TextXAlignment = Computed(function()
-							return if unwrap(textHorizontalScale)==1 then Enum.TextXAlignment.Left else Enum.TextXAlignment.Right
+						TextXAlignment = Scope:Computed(function(use)
+							return if unwrap(textHorizontalScale, use)==1 then Enum.TextXAlignment.Left else Enum.TextXAlignment.Right
 						end),
 						TextTruncate = Enum.TextTruncate.AtEnd,
 						Text = props.Text,
@@ -186,5 +179,5 @@ return function(props: CheckboxProperties): Frame
 		hydrateProps[propertyIndex] = nil
 	end
 
-	return Hydrate(newCheckboxFrame)(hydrateProps)
+	return Scope:Hydrate(newCheckboxFrame)(hydrateProps)
 end
